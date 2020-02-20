b8765fa72542a4d66e35e3352c5704a788e7222c
kaleabmelese <melesekaleab3@gmail.com>
2020-02-20 15:57:46 +0300

    WIP: Here and there

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const dbConfigObj = require('./knexfile')
 var cors = require("cors");
 // require("dotenv").config();
+let dbConnectionConfig
+
+switch (process.env.NODE_ENV){
+  case 'production':
+    dbConnectionConfig = dbConfigObj.production :
+    break;
+  default:
+    dbConnectionConfig = dbConfigObj.development
+}
+
+const appDb = connectToDb(dbConnectionConfig)
+Model.knex(appDb)
 
 const psys = require("./server/api/app");
 const app = express();
--- knexfile.js
+++ knexfile.js
@@ -1,61 +1,21 @@
-// Update with your config settings.
+const devConfig = {
+  client: "postgresql",
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: "./knex/migrations"
+  },
+  seeds: {
+    directory: "./knex/seeds/userseed"
+  },
+  useNullAsDefault: true
+}
+const prodConfig = Object.assign(
+  {},
+  devConfig,
+  { client: 'pg', connection: process.env.DATABASE_URL}
+)
 
 module.exports = {
-  development: {
-    client: "postgresql",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: "./knex/migrations"
-    },
-    seeds: {
-      directory: "./knex/seeds/userseed"
-    },
-    useNullAsDefault: true
-  },
-  test: {
-    client: "pg",
-    connection: {
-      host: "localhost",
-      user: "kaleab",
-      password: "pgpasskal",
-      database: "bettbase"
-    },
-    migrations: {
-      directory: ".knex/migrations"
-    },
-    seeds: {
-      directory: "./knex/seeds/test"
-    },
-    useNullAsDefault: true
-  },
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "bettbase",
-      user: "kaleab",
-      password: "pgpasskal"
-    },
-    pool: {
-      min: 2,
-      max: 10
-      // min: process.env.DATABASE_POOL_MIN,
-      // max: process.env.DATABASE_POOL_MAX
-    },
-    migrations: {
-      tableName: "user",
-      tablename: "predictions",
-      tableName: "freetips"
-    }
-  },
-  production: {
-    client: "postgresql",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  }
-};
+  development: devConfig,
+  production: prodConfig
+}
