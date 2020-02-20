6ea47d200b54611e56b13706c90b2fa1d72d1a23
kaleabmelese <melesekaleab3@gmail.com>
2020-02-20 16:06:28 +0300

    update

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-const dbConfigObj = require('./knexfile')
 var cors = require("cors");
 // require("dotenv").config();
-let dbConnectionConfig
-
-switch (process.env.NODE_ENV){
-  case 'production':
-    dbConnectionConfig = dbConfigObj.production :
-    break;
-  default:
-    dbConnectionConfig = dbConfigObj.development
-}
-
-const appDb = connectToDb(dbConnectionConfig)
-Model.knex(appDb)
 
 const psys = require("./server/api/app");
 const app = express();
