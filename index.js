d199e4a7bc3f4ff2fce61dfba00cdcbb42929d28
kaleabmelese <melesekaleab3@gmail.com>
2020-02-20 15:22:46 +0300

    WIP: port changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use("/api/app", psys);
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`server running at ${port}`);
