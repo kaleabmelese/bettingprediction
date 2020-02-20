36630843c2d7a7b8e26cab208a1ce7c007ef4dd1
kaleabmelese <melesekaleab3@gmail.com>
2020-02-20 15:40:20 +0300

    file cleanup

--- database/dbexec.js
+++ database/dbexec.js
@@ -1,6 +1,3 @@
-// const environment = process.env.NODE_ENV || "development"; // set environment
-// const configuration = require("../knexfile")[environment]; // pull in correct db with env configs
-// const database = require("knex")(configuration); // define database based on above
 const database = require('../dataLogic').connection()
 const dbexec = (module.exports = {
     onlyquery: query => {
