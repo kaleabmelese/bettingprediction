d201aa0d9245ff1018048337d320f0e9cf69c971
kaleabmelese <melesekaleab3@gmail.com>
2020-02-20 15:10:37 +0300

    Added migrate command

--- database/dbexec.js
+++ database/dbexec.js
@@ -1,5 +1,5 @@
-const environment = process.env.NODE_ENV || "development"; // set environment
-const configuration = require("../knexfile")[environment]; // pull in correct db with env configs
+// const environment = process.env.NODE_ENV || "development"; // set environment
+// const configuration = require("../knexfile")[environment]; // pull in correct db with env configs
 // const database = require("knex")(configuration); // define database based on above
 const database = require('../dataLogic').connection()
 const dbexec = (module.exports = {
